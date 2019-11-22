36574cb27bb6f872554fb9ded0a56dddce8f5057
cdntinker <58090022+cdntinker@users.noreply.github.com>
2019-11-22 15:32:41 -0500

    Create TopSecret.h
    Initializations for sensitive information used in code

---
+++ TopSecret.h
@@ -0,0 +1,12 @@
+/* Initializations for sensitive information used in code */
+/* This version is just an example...                     */
+
+/* WiFi Credentials */
+/* If these work as-is on your network, you're an idiot...  :P */
+const char* ssid = "Your_WiFi_SSID";
+const char* password = "Your_WiFi_Password";
+
+/* MQTT Setup */
+const char* MQTTBroker = "URL_of_Your_Broker";
+
+/* I'm sure you get the idea now...  */
